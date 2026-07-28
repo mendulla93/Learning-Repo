5ded2108542c1b5156a5f9db432b83b8ff101b0a
Mendula35 <olaidetunde84@gmail.com>
2026-07-29 07:53:18 +1300

    added new features on script.js file

--- stcript.js
+++ stcript.js
@@ -1,2 +1,7 @@
 let counter = (document.querySelector("input-field").innerHTML = 4);
 console.log("muhammed is learning version control system");
+
+let apiKey = {
+  name: olaidetnde,
+  username: baba,
+};
